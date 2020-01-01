d002dfbd8bb2f253a74a817061d1f16ab06657c7
Piyush Raj <piyush.189303055@muj.manipal.edu>
2020-01-01 15:58:05 +0530

    Fix #4: lightGallery w/ DOM manipulation

--- lab/static/js/sketch.js
+++ lab/static/js/sketch.js
@@ -6,8 +6,8 @@ var count = 0;
 var theta = 0
 var thetacum = 0;
 var p,s,t,q;
-
-
+var title = 'Symphony No. 5 (1st movement)', artist, album, syear;
+var reader = new FileReader();
 
 function setColor(colorSchema) {
     song.stop(); // stop song if playing
@@ -19,6 +19,7 @@ function setColor(colorSchema) {
     t = color.t;
     q = color.q;
     a = 0; // reset the size
+    background(255)
 }
 
 function toggleSong() {
@@ -30,6 +31,22 @@ function toggleSong() {
 }
 
 function scall() {
+  reader.onload = function(e) {
+    var dv = new jDataView(this.result);
+
+    // "TAG" starts at byte -128 from EOF.
+    // See http://en.wikipedia.org/wiki/ID3
+    if (dv.getString(3, dv.byteLength - 128) == 'TAG') {
+      title = dv.getString(30, dv.tell());
+      artist = dv.getString(30, dv.tell());
+      album = dv.getString(30, dv.tell());
+      syear = dv.getString(4, dv.tell());
+    } else {
+      // no ID3v1 data found.
+    }
+  };
+  reader.readAsArrayBuffer(document.getElementById('toggle-song').files[0]);
+
   button.style('display', 'block'); // show the toggle btn
 }
 
@@ -44,6 +61,8 @@ function handleFile(file) {
   var fileURL=  file.data;
   song = loadSound(fileURL, scall); // success callback func
   song.playMode('restart'); // make sure song restarts
+  a = 0; // reset the size
+  background(255)
 }
 
 function setup() {
@@ -54,6 +73,7 @@ function setup() {
   q = '#A61458'
   input = createFileInput(handleFile);
   input.style('position', 'absolute');
+  input.id('toggle-song');
   input.style('top', '0');
   input.style('right', '0');
   
@@ -68,8 +88,6 @@ function setup() {
   fft = new p5.FFT(0, 512);
   amplitude = new p5.Amplitude();
 }
-
-
 
 function draw() {
   var color =[p,s,t,q];
@@ -86,16 +104,42 @@ function draw() {
   stroke(color[constrain(int(size), 0,3)]);
   box(350-a*10);
   a+=0.009;
-  count++;
-  console.log(count);
   if (a>35) {
-  document.getElementById("ret").style.display = 'block';
-  document.getElementById("ret").src=canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); // sets the image
-  clear();
-  a=0;
-  var bb=song.currentTime();
-  console.log(bb, song.duration());
+   count++
+   if (count==1) {
+    document.getElementById("ret-img").src=canvas.toDataURL("image/png"); // sets the image
+    document.getElementById("ret-li").setAttribute('data-src', canvas.toDataURL("image/png"));
+    document.getElementById("ret-li").setAttribute('data-sub-html', "<h4>" + count + ": " + title + "</h4><p>Exported from " + title + " at " + song.currentTime() + " </p>");
+    document.getElementById("ret-li").style.display = 'block';
+    document.getElementById("imsg").style.display = 'block';
+   } else if (count>1) {
+    imgPounder(canvas.toDataURL("image/png")); // dynamic img-pounder
+   }
+   clear(); // clear the canvas()
+   background(255) // again set the bgcolor to white
+   a=0; // reset
+   //noLoop();
+   //console.log(song.currentTime(), song.duration());
   }
   }
   }
+}
+
+const liHTML = `
+        <a href="">
+          <img id="ret-img" class="img-responsive" src="{embedImgSrc}">
+          <div class="lgallery-poster">
+            <img src="/static/img/zoom.png">
+          </div>
+        </a>
+     `;
+
+function imgPounder(src) {
+  var li = document.createElement('li');
+  li.style.display = 'none';
+  li.setAttribute('data-src', src);
+  li.setAttribute('data-responsive', src + " 700");
+  li.setAttribute('data-sub-html', "<h4>" + count + ": " + title + "</h4><p>Exported from " + title + " at " + song.currentTime() + " </p>");
+  li.innerHTML = liHTML.replace(/{embedImgSrc}/g, src);
+  document.getElementById('lightgallery').appendChild(li);
 }
