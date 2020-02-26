452200fe84f48e9b6196db289b20d8e35682a0d1
Piyush Raj <piyush.189303055@muj.manipal.edu>
2020-02-27 04:24:41 +0530

    remove: redundant song file and @app.route('mobile/')

--- src/app.py
+++ src/app.py
@@ -7,9 +7,5 @@ app.config['SECRET_KEY'] = '0x48piraj'
 def index():
     return render_template('index.html')
 
-@app.route('/mobile', methods=['GET'])
-def mobile():
-    return render_template('mobile.html')
-
 if __name__ == "__main__":
     app.run("0.0.0.0", port=8080, debug=True, threaded=True)
